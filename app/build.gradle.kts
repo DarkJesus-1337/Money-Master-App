aa4bbfd7f58d49738a9ab838f072e813245e5dd2
DarkJesus-1337 <s.dendtler@yahoo.com>
2025-07-18 17:12:01 +0200

    release v1.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 1
-        versionName = "1.5.0"
+        versionName = "1.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
