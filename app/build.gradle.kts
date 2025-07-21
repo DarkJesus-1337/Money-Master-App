2947048aa595c3a200d6de38bc2120b8482edb31
DarkJesus-1337 <s.dendtler@yahoo.com>
2025-07-21 15:34:45 +0200

    test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 1
-        versionName = "1.7.6"
+        versionName = "1.7.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
