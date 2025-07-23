867aac4ee55e3f6f0dd6e379db82142f6696b27b
DarkJesus-1337 <s.dendtler@yahoo.com>
2025-07-23 19:09:00 +0200

    feat: new updatesystem integratet, new designn, bugfix etc

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 1
-        versionName = "1.7.7"
+        versionName = "0.9.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
